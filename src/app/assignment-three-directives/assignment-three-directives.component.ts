e8c55c4d9127488d7efd16a843279db2d5b69641
Xiangyu Liu <neulxy@gmail.com>
2022-11-02 14:04:08 +1300

    toggle displaying the paragraph with button click

--- src/app/assignment-three-directives/assignment-three-directives.component.ts
+++ src/app/assignment-three-directives/assignment-three-directives.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AssignmentThreeDirectivesComponent implements OnInit {
   constructor() {}
 
+  displayContent = true;
+
   ngOnInit(): void {}
 }
